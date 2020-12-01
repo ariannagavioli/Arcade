9f29f2a73df0bef96ffefac9512a7055a713e810
Arianna Gavioli <gavioli.arianna@gmail.com>
2020-12-01 21:54:30 +0100

    LCD picures bitmaps

---
+++ pictures.h
@@ -0,0 +1,77 @@
+/*******************************************************************************
+* @file		: pictures.h
+* @author	: IMS Systems LAB & Technical Marketing
+* @version	: V1.0.0
+* @date		: 18-Oct-2020
+* @brief	: Image Declarations
+*******************************************************************************/
+/* @attention
+*
+* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+* TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+* DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+* FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+* CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*
+* <h2><center>&copy; COPYRIGHT 2020 STMicroelectronics</center></h2>
+*******************************************************************************
+*/
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __PICTURES_H
+#define __PICTURES_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types --------------------------------------------------------------*/
+
+/* Exported constants ----------------------------------------------------------*/
+
+
+extern const unsigned char arkanoid_Image[39414];
+
+/* Exported macros -------------------------------------------------------------*/
+/* Exported functions ----------------------------------------------------------*/
+/* Exported types --------------------------------------------------------------*/
+
+/* Exported constants ----------------------------------------------------------*/
+
+
+extern const unsigned char snake_pressed_Image[39414];
+
+/* Exported macros -------------------------------------------------------------*/
+/* Exported functions ----------------------------------------------------------*/
+/* Exported types --------------------------------------------------------------*/
+
+/* Exported constants ----------------------------------------------------------*/
+
+extern const unsigned char arkanoid_pressed_Image[39414];
+
+/* Exported macros -------------------------------------------------------------*/
+/* Exported functions ----------------------------------------------------------*/
+/* Exported types --------------------------------------------------------------*/
+
+/* Exported constants ----------------------------------------------------------*/
+
+extern const unsigned char snake_Image[39414];
+
+/* Exported macros -------------------------------------------------------------*/
+/* Exported functions ----------------------------------------------------------*/
+/* Exported types --------------------------------------------------------------*/
+
+/* Exported constants ----------------------------------------------------------*/
+
+extern const unsigned char background_Image[155574];
+
+/* Exported macros -------------------------------------------------------------*/
+/* Exported functions ----------------------------------------------------------*/
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__PICTURES_H */
+
+/******************* (C) COPYRIGHT 2020 STMicroelectronics *****END OF FILE****/
+
